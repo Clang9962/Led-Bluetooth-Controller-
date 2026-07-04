5e9952ff7a9614766380ebec7becd23896db233a
Clang9962 <7yckh7dyd6@privaterelay.appleid.com>
2026-07-04 03:22:50 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/ble_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("LED Controller")),
-        body: const Center(child: Text("App Running")),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BleProvider()),
+      ],
+      child: MaterialApp(
+        title: 'XC610 Controller',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
     );
   }
